4ec1d0e752028f35223c7d37be544ca907583742
kahlidfma5 <kahlidfma5@hotmail.com>
2025-05-12 07:32:38 +0000

    Secure access to an element in a list

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def element_at(my_list, idx):
     if idx < 0:
         return (None)
@@ -7,5 +8,4 @@ def element_at(my_list, idx):
 
     if idx > length - 1:
         return (None)
-
     return(my_list[idx])
