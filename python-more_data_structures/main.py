48ce9f7cb1f5bb2867188ad6d06ab3da6b498ba9
kahlidfma5 <kahlidfma5@hotmail.com>
2025-05-13 11:41:20 +0000

    Roman to Integer

--- python-more_data_structures/main.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-search_replace = __import__('0-search_replace').search_replace
-
-my_list = [1, 2, 3, 2, 4]
-new_list = search_replace(my_list, 2, 99)
-
-print("Original list:", my_list)
-print("New list:", new_list)
