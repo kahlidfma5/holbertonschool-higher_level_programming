2eb6206663775ce3c7fd67a456f9adc87e5ddc56
kahlidfma5 <kahlidfma5@hotmail.com>
2025-05-28 15:06:18 +0000

    Lookup

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-def lookup(obj):
-
-
-    """ Function that returns the list of available attributes
+""" Function that returns the list of available attributes
         and methods of an object
 
     Args:
@@ -11,5 +8,8 @@ def lookup(obj):
     Returns:
         List of attributes
     """
+
+
+def lookup(obj):
 
     return dir(obj)
