6719bacfbb537c79f9942252e8c7662512345765
kahlidfma5 <kahlidfma5@hotmail.com>
2025-06-24 16:53:05 +0000

    update Temperatures #2

--- SQL_introduction/103-max_state.sql
+++ SQL_introduction/103-max_state.sql
@@ -1,6 +1,8 @@
 -- Script that displays the max temperature of each state
 -- Query to display the max temperature of each state
-SELECT state, MAX(value) AS max_temp
-FROM temperatures
-GROUP BY state
-LIMIT 3;
+SELECT s.name
+FROM states s
+JOIN cities c ON c.state_id = s.id
+GROUP BY s.id
+ORDER BY COUNT(c.id) DESC
+LIMIT 1;
