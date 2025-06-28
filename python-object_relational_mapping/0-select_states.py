26d6336bde5eddf8ea4eb31023abb77181cc0d1f
kahlidfma5 <kahlidfma5@hotmail.com>
2025-06-28 17:28:20 +0000

    update

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
-        host="127.0.0.1",
+        host="localhost",
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
