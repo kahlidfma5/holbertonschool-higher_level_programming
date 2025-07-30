cb21424e91a93e8497eebc1c111d6e66d0626cfb
kahlidfma5 <kahlidfma5@hotmail.com>
2025-07-30 19:53:46 +0000

    add

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,16 +1,8 @@
 #!/usr/bin/env python3
-"""add module
-"""
+"""Write a type-annotated function add that takes a float a and
+a float b as arguments and returns their sum as a float."""
 
 
 def add(a: float, b: float) -> float:
-    """Add two numbers, a and b
-
-    Args:
-        a (float): number
-        b (float): number
-
-    Returns:
-        float: a + b
-    """
+    """returns the sum of two floats"""
     return a + b
